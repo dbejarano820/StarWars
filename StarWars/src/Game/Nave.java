15226e002ce8968e4cbe203e53c82b18824caf60
JosueGupi <josuegupi@gmail.com>
2021-01-30 16:49:27 -0600

    Update Nave.java

--- StarWars/src/Game/Nave.java
+++ StarWars/src/Game/Nave.java
@@ -5,14 +5,25 @@
  */
 package Game;
 
+import static java.lang.Thread.sleep;
+
 /**
  *
  * @author XT
  */
-public class Nave extends Thread{
+public class Nave {
     
-    public static String revelar(Player victima,int x,int y){
+    public static String revelar(Player victima,int x,int y) throws InterruptedException{
         String res = "La nave visito la casilla ("+x+","+y+") del jugador "+victima.nombre+" y revelo un radio de 3 casillas";
+        sleep(90000);
+        for(int i = 0; i < victima.tablero.length;i++){
+            for(int j = 0; j < victima.tablero[i].length;j++){
+                double distancia = Math.sqrt(((x-j)*(x-j))+((y-i)*(y-i)));
+                if(distancia <= 3){
+                    victima.tablero[i][j].revelada = 1;
+                }
+            }
+        }
         
         
         return res;
