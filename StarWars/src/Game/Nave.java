9f70e86d43b5b08b3e4082e1c4d785da2dd62b64
JosueGupi <josuegupi@gmail.com>
2021-01-30 18:25:38 -0600

    Update Nave.java

--- StarWars/src/Game/Nave.java
+++ StarWars/src/Game/Nave.java
@@ -5,13 +5,28 @@
  */
 package Game;
 
+import Server.*;
+import java.io.IOException;
 import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author XT
  */
-public class Nave {
+public class Nave extends Thread{
+    int x;
+    int y;
+    Player victima;
+    ThreadServer hilo;
+
+    public Nave(int x, int y, Player victima) {
+        this.x = x;
+        this.y = y;
+        this.victima = victima;
+    }
+    
     
     public static String revelar(Player victima,int x,int y) throws InterruptedException{
         
@@ -29,6 +44,34 @@ public class Nave {
         
         
         return res;
+    }
+    @Override
+    public void run(){
+        String res = "La nave visito la casilla ("+x+","+y+") del jugador "+victima.nombre+" y revelo un radio de 3 casillas";
+        try {
+            sleep(90000);
+            
+            
+            for(int i = 0; i < victima.tablero.length;i++){
+                for(int j = 0; j < victima.tablero[i].length;j++){
+                    double distancia = Math.sqrt(((x-j)*(x-j))+((y-i)*(y-i)));
+                    
+                    if(distancia <= 3){
+                        victima.tablero[i][j].revelada = 1;
+                    }
+                }
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Nave.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            hilo.mandarConsolaTodas(res);
+        } catch (IOException ex) {
+            Logger.getLogger(Nave.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }
     
 }
